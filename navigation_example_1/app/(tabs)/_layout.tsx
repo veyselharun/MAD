2d03cfe533a5815abd706e1d2ce4bf694d7573ab
Veysel Harun ŞAHİN <vsahin@sakarya.edu.tr>
2025-05-13 10:35:16 +0300

    Add navigation example

---
+++ navigation_example_1/app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="(home)" />
+      <Tabs.Screen name="settings" />
+    </Tabs>
+  );
+}
